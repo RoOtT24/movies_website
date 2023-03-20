55ce702290e9fd828e6c50a63aab33927361f123
RoOt24 <1190579@student.birzeit.edu>
2023-03-20 21:01:56 +0200

    loader & home2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,11 +5,10 @@ export const Home = ({trending}) => {
   return (
     <div className='d-flex justify-content-between flex-wrap'>
    
-  <h1>بعرف انها فاضية ، انتي اشتغلي الي عليكيي وانطزي</h1>
 
- {/* { trending.map( (movie,index)=> 
-   <Card key={index} description={movie.overview} media_type={movie.media_type} title={movie.title} rating={movie.vote_average} img1={movie.poster_path} /> 
-   )} */}
+ { trending?.map( (movie,index)=> 
+   <Card key={index} description={movie.overview} media_type={movie.media_type} title={movie.title} rating={movie.vote_average} img1={movie.poster_path} img2={movie.backdrop_path} /> 
+   )}
     </div>
   )
 }
